b06687c953d5a82c4af4e53cfc7e9a79a2cc28c2
Link4Electronics <link4electronics@gmail.com>
2026-03-10 04:20:42 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/
+quick-sharun ./AppDir/bin/drally_linux
 
 # Additional changes can be done in between here
 
