03b762a9f438e892aead1b4b652a86a7cccf2c34
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:27:29 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,10 +14,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Making nightly build of dRally..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/urxp/dRally"
