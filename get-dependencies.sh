caae25a5f7b68f7dd367100b9c5d589e714459e3
Link4Electronics <link4electronics@gmail.com>
2026-08-19 02:27:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,7 +14,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-echo "Making nightly build of dRally..."
+echo "Building dRally..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/urxp/dRally"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
